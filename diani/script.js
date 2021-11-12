72c10bca7d5c49f2390fb2cd954cde048371ed8b
Elvis Owino <92543072+Elvis-Owino@users.noreply.github.com>
2021-11-13 02:46:28 +0300

    Updated

---
+++ diani/script.js
@@ -0,0 +1,6 @@
+var navigation = document.querySelector('.navigation');
+document.querySelector('.toggler').onclick = function () {
+    this.classList.toggle ('active')
+    navigation.classList.toggle('active') 
+      
+};
