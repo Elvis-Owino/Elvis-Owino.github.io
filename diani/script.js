3c3efbcf45ea6426fae289ef5fe5473ab4ba5c23
Elvis Owino <92543072+Elvis-Owino@users.noreply.github.com>
2021-11-16 15:15:18 +0300

    Updated

--- diani/script.js
+++ diani/script.js
@@ -4,3 +4,21 @@ document.querySelector('.toggler').onclick = function () {
     navigation.classList.toggle('active') 
       
 };
+
+var current=document.querySelector('.current');
+var imgs=document.querySelectorAll('.imgs img');
+var opacity=0.4;
+
+imgs.forEach(img => img.addEventListener('click', imgClick));
+function imgClick(e) {}
+
+imgs[0].style.opacity=opacity;
+
+function imgClick (e){
+   imgs.forEach( img => (img.getElementsByClassName.opacity=1))
+   current.src=e.target.src;
+   current.classList.add('fade-in');
+   setTimeout(()=> current.classList.remove('fade-in'), 500)
+}
+
+
